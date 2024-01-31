88c4186341da4d996e3355b5e8ae351933dddde7
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-01-31 18:56:20 +0400

    Set redirect to /buy?name instead of /buy/name

--- frontend/src/pages/buy/[name].tsx
+++ frontend/src/pages/buy/[name].tsx
@@ -1,35 +1,11 @@
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import Head from 'next/head'
-import { useTranslation } from 'next-i18next'
-import { BuyForm } from '@/widgets/buy/buy-form'
+import { GetServerSidePropsContext, GetServerSidePropsResult } from 'next'
 
-export default function BuyNamePage() {
-  const { t } = useTranslation('buy')
-  
-  return (
-    <>
-      <Head>
-        <title>{t('title')}</title>
-        <meta name="description" content={t('description')} />
-        <meta name="ogp:title" content={t('title')} />
-        <meta name="ogp:description" content={t('description')} />
-        <meta name="ogp:sitename" content='ONS Registry' />
-      </Head>
-      <main
-        className='flex min-h-screen flex-col items-center justify-between antialiased font-semibold pb-24'
-      >
-        <BuyForm />
-      </main>
-    </>
-  )
-}
-
-export async function getServerSideProps({ locale }: { locale: string }) {
+export function getServerSideProps(context: GetServerSidePropsContext<{ name: string }>): GetServerSidePropsResult<Record<string, never>> {
   return {
-    props: {
-      ...(await serverSideTranslations(locale, [
-        'buy', 'footer'
-      ])),
-    },
+    props: {},
+    redirect: {
+      destination: '/buy?' + context.params?.name,
+      permanent: false,
+    }
   }
 }
---
+++ frontend/src/pages/buy/index.tsx
@@ -0,0 +1,35 @@
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import Head from 'next/head'
+import { useTranslation } from 'next-i18next'
+import { BuyForm } from '@/widgets/buy/buy-form'
+
+export default function BuyNamePage() {
+  const { t } = useTranslation('buy')
+
+  return (
+    <>
+      <Head>
+        <title>{t('title')}</title>
+        <meta name="description" content={t('description')} />
+        <meta name="ogp:title" content={t('title')} />
+        <meta name="ogp:description" content={t('description')} />
+        <meta name="ogp:sitename" content='ONS Registry' />
+      </Head>
+      <main
+        className='flex min-h-screen flex-col items-center justify-between antialiased font-semibold pb-24'
+      >
+        <BuyForm />
+      </main>
+    </>
+  )
+}
+
+export async function getServerSideProps({ locale }: { locale: string }) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, [
+        'buy', 'footer'
+      ])),
+    },
+  }
+}
