660ac8c77addaaabd319aac0ee7d4ba91297fdd6
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2024-03-10 19:04:45 +0400

    Fix session-receipts

--- backend/src/session-receipts.ts
+++ backend/src/session-receipts.ts
@@ -4,7 +4,7 @@ export async function sendReceiptToSession(sessionID: string, content: {
   try {
     let text
     if (content.language === 'ru') {
-      text = content.name + `, поздравляем с покупкой имени на https://ons.sessionbots.directory/ 🎉 Ваше имя уже активно и вас уже можно найти по нему в Session. Запись о покупке в блокчейне: https://oxen.observer/tx/${content.txHash} Если вы еще не можете перейти по вашему имени, подождите до 20 минут для подтверждения блокчейном.`
+      text = content.name + `, поздравляем с покупкой имени на https://ons.sessionbots.directory/ru/buy 🎉 Ваше имя уже активно и вас уже можно найти по нему в Session. Запись о покупке в блокчейне: https://oxen.observer/tx/${content.txHash} Если вы еще не можете перейти по вашему имени, подождите до 20 минут для подтверждения блокчейном.`
 
       if ('seedPhrase' in content) {
         text += '\n\nЕсли вы захотите управлять своим именем (например, привязать это имя к другому Session ID), вам потребуется установить официальное приложение OXEN Wallet и ввести туда эту мнемоническую фразу для доступа к кошельку, с которого был куплен маппинг: ' + content.seedPhrase + '. НИКОМУ НЕ ПОКАЗЫВАЙТЕ ЭТУ ФРАЗУ — она дает доступ к купленному вами имени в блокечейне и с помощью неё можно перепривязать никнейм к другому аккаунту.'
@@ -19,18 +19,18 @@ export async function sendReceiptToSession(sessionID: string, content: {
 
       text += '\n\nСпасибо за покупку и ждем вас снова!'
     } else {
-      text = content.name + `, congratulations on your purchase at https://ons.sessionbots.directory/ 🎉! Your name is already active and you can already be found by it in Session. Here is the blockchain purchase record: https://oxen.observer/tx/${content.txHash} If you still can't go to your nickname, please allow up to 20 minutes for confirmation by the OXEN blockchain).`
+      text = content.name + `, congratulations on your purchase at https://ons.sessionbots.directory/buy 🎉! Your name is already active and you can already be found by it in Session. Here is the blockchain purchase record: https://oxen.observer/tx/${content.txHash} If you still can't go to your nickname, please allow up to 20 minutes for confirmation by the OXEN blockchain.`
 
       if ('seedPhrase' in content) {
         text = '\n\nIf you want to manage your name (for example, bind this name to another Session ID), you will need to install the official OXEN Wallet app and enter this see phrase (mnemonic) there: ' + content.seedPhrase + '. DO NOT SHARE THIS PHRASE WITH ANYONE - it gives access to the name you just bought. '
       } else {
-        text += `\n\nWhen purchasing, you specified your own OXEN wallet, to which the nickname was linked: ${content.ownerOxen}. Remember that anyone having access to this OXEN wallet can rebind your nickname to another Session account, so do not share access to this wallet to anyone.`
+        text += `\n\nWhen making the purchase, you specified your own OXEN wallet, to which the nickname was linked: ${content.ownerOxen}. Remember that anyone having access to this OXEN wallet can rebind your nickname to another Session account, so do not share access to this wallet to anyone.`
       }
 
       text += ' '
       text += 'Please note that we are not affiliated with OXEN, Session and cannot control the blockchain, as well as help with issues related to this. Our site does not support managing your name after purchase.'
 
-      text += '\n\nIf you have any questions, please contact us at Telegram: @hlothdev (preferrably) or by e-mail: onspurchase@hloth.dev or by Session nickname: hloth (slow to response). This account is used to automatically send receipts, so please do not send messages to it — they won\'t be delivered and seen.'
+      text += '\n\nIf you have any questions, please contact us at Telegram: @hlothdev (preferrably) or by e-mail: onspurchase@hloth.dev or by Session nickname: hloth (slowest). This account is used to automatically send receipts, so please do not send messages to it — they won\'t be delivered and seen.'
 
       text += '\n\nThank you for your purchase and we\'re hoping to see you again soon!'
     }
--- session-receipts/src/index.ts
+++ session-receipts/src/index.ts
@@ -1,6 +1,24 @@
 import './env.js'
 import Fastify from 'fastify'
+import { createIdentity, getSessionID, initializeSession, sendMessage } from 'session-messenger-nodejs'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url)) + '/'
+
 const fastify = Fastify({ logger: true })
+await initializeSession({
+  profileDataPath: __dirname + '../session-data'
+})
+try {
+  await getSessionID()
+} catch(e) {
+  if (e.message === 'User is not authorized') {
+    await createIdentity('ons.sessionbots.directory (do not reply)')
+  } else {
+    throw e
+  }
+}
 
 fastify.post('/receipt', {
   schema: {
@@ -13,8 +31,15 @@ fastify.post('/receipt', {
     }
   },
   handler: async function handler(request, reply) {
+    const body = request.body as {
+      sessionID: string
+      text: string
+    }
     try {
-      
+      await sendMessage(body.sessionID, {
+        body: body.text
+      })
+      return { ok: true }
     } catch(e) {
       return { ok: false, error: e.message }
     }
